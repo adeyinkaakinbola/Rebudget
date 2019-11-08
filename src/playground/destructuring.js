9e60443278f7239deaed2b42fecbada6e2f1608f
adeyinkaakinbola <ashleyakinbola@gmail.com>
2019-11-08 09:17:57 +0300

    implement react and redux

---
+++ src/playground/destructuring.js
@@ -0,0 +1,16 @@
+//OBJECT DESTRUCTING
+const person = {
+    name: 'Venice',
+    age:25,
+    location:{
+        city:'Miami',
+        temp:95
+    }
+};
+const { name }= person;
+// console.log(name);
+//ARRAY DESTRUCTURING
+const address=['1000 Juniper St','Berlin','Germany','191AT'];
+// console.log(address[1]);
+const [street,myspot,place,code]=address;
+console.log(myspot);
