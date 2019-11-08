7dbab96d44ffc4cd71555c447a9e88c5ee2e1e9b
adeyinkaakinbola <ashleyakinbola@gmail.com>
2019-11-08 09:15:38 +0300

    implement react and redux

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,42 +1,77 @@
 import{ createStore} from 'redux';
 
-const store =createStore((state={count:0},action)=>{
-   switch (action.type){
-        case'INCREMENT':
-        return{
-             count:state.count+1
-        };
-          case'DECREMENT':
-          return{
-               count:state.count-1}
-        case 'RESET':
-             return{
-                  count:0
-             };
-        default:
-             return state;
-   }
+//Action creators - functions that return objects/actions
+const incrementCount =(payload = {}) =>{
+     return{
+          type:'INCREMENT',
+          incrementBy: typeof payload.incrementBy === 'number' ? payload.incrementBy: 1
+     };
+};
 
-});
+const decrementCount = (payload = {}) =>{
+     return{
+          type:'DECREMENT',
+          decrementBy: typeof payload.decrementBy === 'number' ? payload.decrementBy: 1
+     };
+};
+
+const setCount =(payload)=>{
+     return{
+          type:'SET',
+          count: payload.count
+     };
+};
+
+const resetCount = ()=>{
+     return{
+          type:'RESET'
+     }
+};
+//Reducers
+//-reducers are pure functions
+const countReducer=(state={count:0},action)=>{
+          switch (action.type){
+               case'INCREMENT':
+               return{
+                    count:state.count +action.incrementBy
+               };
+                    
+                 case'DECREMENT':
+            
+                 return{
+                      count:state.count-action.decrementBy
+                 };
+                 case 'SET':
+                      return{
+                           count:action.count
+                      };
+               case 'RESET':
+                    return{
+                         count:0
+                    };
+               default:
+                    return state;
+          }
+       
+       }
+
+const store =createStore(countReducer);
+store.subscribe(()=>{
  console.log(store.getState());
+});
 
  // Actions-an object that gets sent to the store an action must have a type
 
 
  //increment count
- store.dispatch({
-      type:'INCREMENT'
- });
+ store.dispatch(incrementCount({incrementBy:5}));
 
- store.dispatch({
-     type:'INCREMENT'
-});
+ store.dispatch(incrementCount());
 
-store.dispatch({
-     type:'RESET'
-});
+store.dispatch(resetCount());
 
-store.dispatch({
-     type:'DECREMENT'
-});
- console.log(store.getState());
+store.dispatch(decrementCount());
+
+store.dispatch(decrementCount({ decrementBy:10}));
+
+ store.dispatch(setCount({count:101}));
